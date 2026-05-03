9188b399134aa6da118ab180c3c7965a8f18ff34
Jacob W <222jacobw@gmail.com>
2026-05-03 11:40:31 -0500

    Edits

--- custom-vars.el
+++ custom-vars.el
@@ -4,93 +4,182 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
+ '(c-default-style '((java-mode . "java") (awk-mode . "awk") (other . "user")))
  '(company-abort-on-unique-match t)
  '(custom-enabled-themes '(nano-dark))
  '(custom-safe-themes
-   '("d2ab3d4f005a9ad4fb789a8f65606c72f30ce9d281a9e42da55f7f4b9ef5bfc6"
-	 "921f165deb8030167d44eaa82e85fcef0254b212439b550a9b6c924f281b5695"
-	 "e14289199861a5db890065fdc5f3d3c22c5bac607e0dbce7f35ce60e6b55fc52"
-	 "acfe7ff6aacb9432f124cde4e35d6d2b4bc52916411de73a6ccded9750c9fa97"
-	 "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02"
-	 "e8bd9bbf6506afca133125b0be48b1f033b1c8647c628652ab7a2fe065c10ef0"
-	 "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
-	 "0d2c5679b6d087686dcfd4d7e57ed8e8aedcccc7f1a478cd69704c02e4ee36fe"
-	 "d12b1d9b0498280f60e5ec92e5ecec4b5db5370d05e787bc7cc49eae6fb07bc0"
-	 "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
-	 "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
-	 "b754d3a03c34cfba9ad7991380d26984ebd0761925773530e24d8dd8b6894738"
-	 "f253a920e076213277eb4cbbdf3ef2062e018016018a941df6931b995c6ff6f6"
-	 "2f7fa7a92119d9ed63703d12723937e8ba87b6f3876c33d237619ccbd60c96b9"
-	 "a6920ee8b55c441ada9a19a44e9048be3bfb1338d06fc41bce3819ac22e4b5a1"
-	 "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4"
-	 "3613617b9953c22fe46ef2b593a2e5bc79ef3cc88770602e7e569bbd71de113b"
-	 "87fa3605a6501f9b90d337ed4d832213155e3a2e36a512984f83e847102a42f4"
-	 "42a6583a45e0f413e3197907aa5acca3293ef33b4d3b388f54fa44435a494739"
-	 "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a"
-	 "1bc640af8b000ae0275dbffefa2eb22ec91f6de53aca87221c125dc710057511"
-	 "e4d4cc443964b8a64defc06de3edb2363f7cb1b3c3ae2272b2c1487f626e4318"
-	 "19d62171e83f2d4d6f7c31fc0a6f437e8cec4543234f0548bad5d49be8e344cd"
-	 "a368631abdadffb6882f9994637d7216167912311447f1ec02f9dc58e9cc62a9"
-	 "72d9086e9e67a3e0e0e6ba26a1068b8b196e58a13ccaeff4bfe5ee6288175432"
-	 "4d714a034e7747598869bef1104e96336a71c3d141fa58618e4606a27507db4c"
-	 "3f24dd8f542f4aa8186a41d5770eb383f446d7228cd7a3413b9f5e0ec0d5f3c0"
-	 "b9761a2e568bee658e0ff723dd620d844172943eb5ec4053e2b199c59e0bcc22"
-	 "1f8bd4db8280d5e7c5e6a12786685a7e0c6733b0e3cf99f839fb211236fb4529"
-	 "088cd6f894494ac3d4ff67b794467c2aa1e3713453805b93a8bcb2d72a0d1b53"
-	 "fffef514346b2a43900e1c7ea2bc7d84cbdd4aa66c1b51946aade4b8d343b55a"
-	 "f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725"
-	 "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0"
-	 "df6dfd55673f40364b1970440f0b0cb8ba7149282cf415b81aaad2d98b0f0290"
-	 "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd"
-	 "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7"
-	 "2f8af2a3a2fae6b6ea254e7aab6f3a8b5c936428b67869cef647c5f8e7985877"
-	 "6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
-	 "dd4582661a1c6b865a33b89312c97a13a3885dc95992e2e5fc57456b4c545176"
-	 "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
-	 "b7a09eb77a1e9b98cafba8ef1bd58871f91958538f6671b22976ea38c2580755"
-	 "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad"
-	 "e1df746a4fa8ab920aafb96c39cd0ab0f1bac558eff34532f453bd32c687b9d6"
-	 "4b88b7ca61eb48bb22e2a4b589be66ba31ba805860db9ed51b4c484f3ef612a7"
-	 "c3c135e69890de6a85ebf791017d458d3deb3954f81dcb7ac8c430e1620bb0f1"
-	 "dfb1c8b5bfa040b042b4ef660d0aab48ef2e89ee719a1f24a4629a0c5ed769e8"
-	 "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f"
-	 "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098"
-	 "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
-	 "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9"
-	 "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1"
-	 "599f72b66933ea8ba6fce3ae9e5e0b4e00311c2cbf01a6f46ac789227803dd96"
-	 "22a0d47fe2e6159e2f15449fcb90bbf2fe1940b185ff143995cc604ead1ea171"
-	 "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
-	 "83550d0386203f010fa42ad1af064a766cfec06fc2f42eb4f2d89ab646f3ac01"
-	 "5244ba0273a952a536e07abaad1fdf7c90d7ebb3647f36269c23bfd1cf20b0b8"
-	 "9b9d7a851a8e26f294e778e02c8df25c8a3b15170e6f9fd6965ac5f2544ef2a9"
-	 "166a2faa9dc5b5b3359f7a31a09127ebf7a7926562710367086fcc8fc72145da"
-	 "7de64ff2bb2f94d7679a7e9019e23c3bf1a6a04ba54341c36e7cf2d2e56e2bcc"
-	 "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19"
-	 "720838034f1dd3b3da66f6bd4d053ee67c93a747b219d1c546c41c4e425daf93"
-	 "b8bd60a23b9e2f08b0c437231ee84f2dacc70fdc4d5a0fb87229bb9926273fdd"
-	 "4d66d185c52e429e814f98265ee34b314bf0ea21a9c0bd020ef406e9f37c15a6"
-	 "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
-	 "01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
-	 "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0"
-	 default))
+   '("9819b94dc38ee935362d4bf1c1198ebb8ebeabb5e8c85a78436565720d91c0d7"
+     "1eaf5b063f0a33436f094ac9866a05d7bc8488acdf661110516f9a796d3471af"
+     "ee10ae769b2f1383371a1e271ee1ddbed2bd9969331cc9417921f4251efbce0f"
+     "106fadeab4fb8cf50eeae1e1fd051ae00d0e71a40b5b0f0f5b9db85015398a61"
+     "b3440bf57d65eeb0cce0c45f91a14b336fe5d2d33d15782c701fa092fa34bf59"
+     "366f7fb70999d739ff559568b011f8cfb05c88124e4c944e5e1008312e572137"
+     "d422edfdab7b5f84fb305732eaa36963b890acbd5cc88233309c042b8e967197"
+     "f06a77b1c3433502f0f1db69c0b7a4c686c78d43d7cbfd486ca39d8da983f832"
+     "eccbc5afab954d8d4cd7748921cc93c2e770eb8059cb43bf2ef3bcbb4a9aca51"
+     "6b9fbe5d88424ac7283b8f36b6f184d1140fcd4bfcab1f72a3c58c48dc254bae"
+     "26a8e9035bda6d46d95bbf224ac52e90eb5befd80f5a083b72865697c8a81304"
+     "1fa665965395c909dc60fc35b66a2ad902904334cf2b9beb2b07dddab67c782a"
+     "b5beaec9ea4c8d0b3a104b4bf9ce4e7519a4a5a045527af8e6a93df85111048c"
+     "4bc34187baf114f1f3de085ffe9510b3c43fe505d21bd52d75bd5aade8c7839e"
+     "00b98b7447d11b4c3bc37588014459a764e8fb3ad3504443af69ead9a5dbc27d"
+     "b5ac0a656be7115492b8a1e39eabac55c2f0fb267577f7f5406278a7ed1198c5"
+     "d8541a7c1ca4117b056b34ddf9ed5a6e2547eaf554dc0e7f7b6e15ed14490a37"
+     "98f2052cba13ccbf7dc91e6ce4dbc02fc9f6421782c11f8403504222f4d423f7"
+     "ed2efc874021b54144b9ed9874e4d9c036090488aeabe19c9ac28576a162ec12"
+     "1b74dd5576787105a0efcd6e8fed74755bb3fd234aaa5d500e21ddbd56c2661a"
+     "2c7dc80264de0ba9409d4ebb3c7b31cf8e4982015066174c786f16a672db71b2"
+     "516ec39655c85f346393f5d93e0f03602b6bfc33335bf2fd673016c9c4cdc69e"
+     "22faff66975354a3dd145ae13934758d45930ad09cbbe8bd1ed74224e4aa5684"
+     "0a8cf72fd94bfb67dd72dc085538b39ea47aeae8bfc2b8545c0d3c99c339c204"
+     "546f3e8c4cb46043df1f646322c4b57049fc4c31fdf96e41db077c3408660057"
+     "578002ec7093cedeb7d279794492b19ad7413cca3e16286e776168eb544798c4"
+     "1b7e575c6681e66d8d83634c2c160b40af12f3756360a4dd81b8032f4495cb5e"
+     "70c88c01b0b5fde9ecf3bb23d542acba45bb4c5ae0c1330b965def2b6ce6fac3"
+     "38b43b865e2be4fe80a53d945218318d0075c5e01ddf102e9bec6e90d57e2134"
+     "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015"
+     "1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518"
+     "b99ff6bfa13f0273ff8d0d0fd17cc44fab71dfdc293c7a8528280e690f084ef0"
+     "c9d837f562685309358d8dc7fccb371ed507c0ae19cf3c9ae67875db0c038632"
+     "4d5d11bfef87416d85673947e3ca3d3d5d985ad57b02a7bb2e32beaf785a100e"
+     "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478"
+     "7c3d62a64bafb2cc95cd2de70f7e4446de85e40098ad314ba2291fc07501b70c"
+     "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0"
+     "93011fe35859772a6766df8a4be817add8bfe105246173206478a0706f88b33d"
+     "9e5e0ff3a81344c9b1e6bfc9b3dcf9b96d5ec6a60d8de6d4c762ee9e2121dfb2"
+     "e8ceeba381ba723b59a9abc4961f41583112fc7dc0e886d9fc36fa1dc37b4079"
+     "5ec088e25ddfcfe37b6ae7712c9cb37fd283ea5df7ac609d007cafa27dab6c64"
+     "d43860349c9f7a5b96a090ecf5f698ff23a8eb49cd1e5c8a83bb2068f24ea563"
+     "77fff78cc13a2ff41ad0a8ba2f09e8efd3c7e16be20725606c095f9a19c24d3d"
+     "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33"
+     "d2ab3d4f005a9ad4fb789a8f65606c72f30ce9d281a9e42da55f7f4b9ef5bfc6"
+     "921f165deb8030167d44eaa82e85fcef0254b212439b550a9b6c924f281b5695"
+     "e14289199861a5db890065fdc5f3d3c22c5bac607e0dbce7f35ce60e6b55fc52"
+     "acfe7ff6aacb9432f124cde4e35d6d2b4bc52916411de73a6ccded9750c9fa97"
+     "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02"
+     "e8bd9bbf6506afca133125b0be48b1f033b1c8647c628652ab7a2fe065c10ef0"
+     "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
+     "0d2c5679b6d087686dcfd4d7e57ed8e8aedcccc7f1a478cd69704c02e4ee36fe"
+     "d12b1d9b0498280f60e5ec92e5ecec4b5db5370d05e787bc7cc49eae6fb07bc0"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
+     "b754d3a03c34cfba9ad7991380d26984ebd0761925773530e24d8dd8b6894738"
+     "f253a920e076213277eb4cbbdf3ef2062e018016018a941df6931b995c6ff6f6"
+     "2f7fa7a92119d9ed63703d12723937e8ba87b6f3876c33d237619ccbd60c96b9"
+     "a6920ee8b55c441ada9a19a44e9048be3bfb1338d06fc41bce3819ac22e4b5a1"
+     "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4"
+     "3613617b9953c22fe46ef2b593a2e5bc79ef3cc88770602e7e569bbd71de113b"
+     "87fa3605a6501f9b90d337ed4d832213155e3a2e36a512984f83e847102a42f4"
+     "42a6583a45e0f413e3197907aa5acca3293ef33b4d3b388f54fa44435a494739"
+     "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a"
+     "1bc640af8b000ae0275dbffefa2eb22ec91f6de53aca87221c125dc710057511"
+     "e4d4cc443964b8a64defc06de3edb2363f7cb1b3c3ae2272b2c1487f626e4318"
+     "19d62171e83f2d4d6f7c31fc0a6f437e8cec4543234f0548bad5d49be8e344cd"
+     "a368631abdadffb6882f9994637d7216167912311447f1ec02f9dc58e9cc62a9"
+     "72d9086e9e67a3e0e0e6ba26a1068b8b196e58a13ccaeff4bfe5ee6288175432"
+     "4d714a034e7747598869bef1104e96336a71c3d141fa58618e4606a27507db4c"
+     "3f24dd8f542f4aa8186a41d5770eb383f446d7228cd7a3413b9f5e0ec0d5f3c0"
+     "b9761a2e568bee658e0ff723dd620d844172943eb5ec4053e2b199c59e0bcc22"
+     "1f8bd4db8280d5e7c5e6a12786685a7e0c6733b0e3cf99f839fb211236fb4529"
+     "088cd6f894494ac3d4ff67b794467c2aa1e3713453805b93a8bcb2d72a0d1b53"
+     "fffef514346b2a43900e1c7ea2bc7d84cbdd4aa66c1b51946aade4b8d343b55a"
+     "f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725"
+     "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0"
+     "df6dfd55673f40364b1970440f0b0cb8ba7149282cf415b81aaad2d98b0f0290"
+     "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd"
+     "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7"
+     "2f8af2a3a2fae6b6ea254e7aab6f3a8b5c936428b67869cef647c5f8e7985877"
+     "6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
+     "dd4582661a1c6b865a33b89312c97a13a3885dc95992e2e5fc57456b4c545176"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
+     "b7a09eb77a1e9b98cafba8ef1bd58871f91958538f6671b22976ea38c2580755"
+     "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad"
+     "e1df746a4fa8ab920aafb96c39cd0ab0f1bac558eff34532f453bd32c687b9d6"
+     "4b88b7ca61eb48bb22e2a4b589be66ba31ba805860db9ed51b4c484f3ef612a7"
+     "c3c135e69890de6a85ebf791017d458d3deb3954f81dcb7ac8c430e1620bb0f1"
+     "dfb1c8b5bfa040b042b4ef660d0aab48ef2e89ee719a1f24a4629a0c5ed769e8"
+     "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f"
+     "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098"
+     "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9"
+     "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1"
+     "599f72b66933ea8ba6fce3ae9e5e0b4e00311c2cbf01a6f46ac789227803dd96"
+     "22a0d47fe2e6159e2f15449fcb90bbf2fe1940b185ff143995cc604ead1ea171"
+     "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
+     "83550d0386203f010fa42ad1af064a766cfec06fc2f42eb4f2d89ab646f3ac01"
+     "5244ba0273a952a536e07abaad1fdf7c90d7ebb3647f36269c23bfd1cf20b0b8"
+     "9b9d7a851a8e26f294e778e02c8df25c8a3b15170e6f9fd6965ac5f2544ef2a9"
+     "166a2faa9dc5b5b3359f7a31a09127ebf7a7926562710367086fcc8fc72145da"
+     "7de64ff2bb2f94d7679a7e9019e23c3bf1a6a04ba54341c36e7cf2d2e56e2bcc"
+     "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19"
+     "720838034f1dd3b3da66f6bd4d053ee67c93a747b219d1c546c41c4e425daf93"
+     "b8bd60a23b9e2f08b0c437231ee84f2dacc70fdc4d5a0fb87229bb9926273fdd"
+     "4d66d185c52e429e814f98265ee34b314bf0ea21a9c0bd020ef406e9f37c15a6"
+     "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
+     "01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
+     "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0"
+     default))
  '(display-line-numbers 'visual)
- '(global-whitespace-mode nil)
+ '(eglot-autoshutdown t)
+ '(eglot-booster-io-only t)
+ '(eglot-ignored-server-capabilities '(:codeActionProvider :inlayHintProvider))
+ '(eldoc-documentation-strategy 'eldoc-documentation-compose)
+ '(eldoc-echo-area-prefer-doc-buffer nil)
+ '(eldoc-echo-area-use-multiline-p t)
+ '(exec-path
+   '("/usr/bin" "/bin" "/usr/sbin" "/sbin"
+     "/Applications/Emacs.app/Contents/MacOS/libexec"
+     "/opt/homebrew/bin"))
+ '(flycheck-highlighting-mode 'symbols)
+ '(global-auto-revert-mode nil)
+ '(global-flycheck-eglot-mode t)
+ '(global-whitespace-mode t)
+ '(indent-tabs-mode nil)
+ '(lsp-eldoc-render-all nil)
  '(make-backup-files t)
- '(org-agenda-files '("/Users/jacobwong/org/todo.org"))
+ '(meow-use-clipboard t)
+ '(org-agenda-files '("/Users/jacobwong/roamBank/20260202202309-tasks.org"))
+ '(org-appear-autolinks t)
+ '(org-appear-inside-latex t)
  '(org-capture-templates
-   '(("t" "Todo item" entry (file "~/org/todo.org") "* TODO %?"
-	  :empty-lines 1 :time-prompt t)))
- '(package-selected-packages nil)
+   '(("t" "Todo item" entry (file "~/org/todo.org") "* todo %?"
+      :empty-lines 1)
+     ("p" "Project planning" entry (file "~/org/project.org") "* %?"
+      :empty-lines-after 1 :time-prompt t)))
+ '(org-export-with-toc nil)
+ '(org-hide-emphasis-markers t)
+ '(org-html-postamble nil)
+ '(org-image-actual-width nil)
+ '(org-roam-capture-templates
+   '(("d" "default" plain "%?" :target
+      (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\12")
+      :unnarrowed t)))
+ '(package-selected-packages
+   '(arduino-mode cape corfu-prescient doom-themes doric-themes eat
+                  ef-themes eglot eglot-booster emacsql embark-consult
+                  emmet-mode exec-path-from-shell flx flycheck-eglot
+                  go-mode god-mode gruber-darker-theme hl-todo magit
+                  marginalia meow modus-themes nano-agenda nano-theme
+                  orderless org-appear org-bullets org-download
+                  org-hyperscheduler pdf-tools projectile python-mode
+                  request rust-mode simple-httpd smartparens surround
+                  typst-preview undo-tree vertico-prescient vterm
+                  vundo web-server which-key xterm-color yasnippet))
+ '(package-vc-selected-packages
+   '((lambda-themes :vc-backend Git :url
+                    "https://github.com/Lambda-Emacs/lambda-themes")
+     (eglot-booster :vc-backend Git :url
+                    "https://github.com/jdtsmith/eglot-booster")))
  '(scroll-conservatively 101)
  '(scroll-margin 8)
- '(whitespace-style
-   '(face trailing tabs spaces space-after-tab::tab
-		  space-after-tab::space space-after-tab space-before-tab::tab
-		  space-before-tab::space space-before-tab space-mark tab-mark)))
+ '(select-enable-clipboard nil)
+ '(sh-shell-file "/opt/homebrew/bin/zsh")
+ '(shell-file-name "/opt/homebrew/bin/zsh")
+ '(whitespace-style '(face trailing tabs spaces space-mark tab-mark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(whitespace-space ((t (:foreground "gray40"))))
+ '(whitespace-tab ((t (:foreground "gray40")))))
--- init.el
+++ init.el
@@ -10,10 +10,14 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (column-number-mode 1)
+(setopt auto-revert-avoid-polling t)
 (scroll-bar-mode -1)
+(setopt sentence-end-double-space nil)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 ;; (global-display-line-numbers-mode 1)
 (set-frame-font "personal iosevka 16" t)
+(setq nano-font-family-monospaced "personal iosevka")
+(setq nano-font-size 16)
 (setq split-width-threshold 9999)
 (setq split-height-threshold 0)
 (recentf-mode 1)
@@ -22,6 +26,8 @@
 (global-font-lock-mode 1)
 (blink-cursor-mode -1)
 (keymap-global-set "C-c c" 'org-capture)
+(setq mac-command-modifier 'meta)
+(require 'org-download)
 (use-package orderless
   :ensure t
   :custom
@@ -76,12 +82,12 @@
 (when (string= system-type "windows-nt")
   (cd "C:/Users/Jacob/"))
 
-;; (electric-pair-mode)
+(electric-pair-mode t)
 
-;; (use-package nano-theme
-;;   :ensure t
-;;   :init
-;;   (load-theme 'nano-dark t))
+(use-package nano-theme
+  :ensure t
+  :init)
+
 
 (use-package doom-themes
   :ensure t
@@ -95,10 +101,19 @@
 
 (use-package corfu
   :ensure t
+  :custom
+  (corfu-cycle t)
+  (corfu-preselect 'insert)
+  :bind
+  (:map corfu-map
+              ("TAB" . corfu-next)
+              ([tab] . corfu-next)
+              ("S-TAB" . corfu-previous)
+              ([backtab] . corfu-previous))
   :init
   (global-corfu-mode)
   (corfu-history-mode))
-  ;; :bind ("C-x C-o" . ))
+;; :bind ("C-x C-o" . ))
 
 (use-package cape
   :ensure t
@@ -119,6 +134,7 @@
   (add-hook 'completion-at-point-functions #'cape-file)
   (add-hook 'completion-at-point-functions #'cape-elisp-block)
   (add-hook 'completion-at-point-functions #'cape-history)
+  (add-hook 'completion-at-point-functions #'cape-keyword)
   ;; ...
 )
 
@@ -132,7 +148,7 @@
 (use-package emacs
   :custom
   ;; TAB cycle if there are only few candidates
-  ;; (completion-cycle-threshold 3)
+  ;; (completion-cycle-threshold t)
 
   ;; Enable indentation+completion using the TAB key.
   ;; `completion-at-point' is often bound to M-TAB.
@@ -174,8 +190,8 @@
 (use-package magit
   :ensure t)
 
-(use-package undo-tree
-  :ensure t)
+;; (use-package undo-tree
+;;   :ensure t)
 
 ;; (use-package evil
 ;;   :ensure t
@@ -183,14 +199,14 @@
 ;;   (evil-mode 1))
 
 (require 'view)
-(keymap-global-set "C-v" (lambda () (interactive) (View-scroll-half-page-forward) (recenter)))
-(keymap-global-set "M-v" (lambda () (interactive) (View-scroll-half-page-backward) (recenter)))
+(keymap-global-set "C-v" (lambda () (interactive) (View-scroll-page-forward) (recenter)))
+(keymap-global-set "M-v" (lambda () (interactive) (View-scroll-page-backward) (recenter)))
 
-;; (use-package projectile
-;;   :ensure t
-;;   :init
-;;   (projectile-mode +1)
-;;   (keymap-global-set "C-c p" 'projectile-command-map))
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  (keymap-global-set "C-c p" 'projectile-command-map))
 
 (use-package flx
   :ensure t)
@@ -210,39 +226,229 @@
 ;;   :config
 ;;   (fussy-setup))
 
-(use-package smartparens
-  :ensure t
-  :init
-  (smartparens-global-mode 1))
+;; (use-package smartparens
+;;   :ensure t
+;;   :init
+;;   (smartparens-global-mode 1))
 
 (use-package prescient
   :ensure t
-  :init)
+  :config
+  (prescient-persist-mode 1))
 
 (use-package corfu-prescient
   :ensure t
-  :init
+  :after corfu prescient
+  :custom
+  (corfu-prescient-enable-sorting t)
+  (corfu-prescient-override-sorting nil)
+  (corfu-prescient-enable-filtering nil)
+  :config
   (corfu-prescient-mode 1))
 
-(use-package god-mode
-  :ensure t
-  :init
-  (setq god-exempt-major-modes nil)
-  (setq god-exempt-predicates nil)
-  (god-mode-all)
-  (define-key god-local-mode-map (kbd "i") #'god-local-mode)
-  (global-set-key (kbd "<escape>") #'(lambda () (interactive) (god-local-mode 1)))
-  (define-key god-local-mode-map (kbd ".") #'repeat))
+;; (use-package god-mode
+;;   :ensure t
+;;   :init
+;;   (setq god-exempt-major-modes nil)
+;;   (setq god-exempt-predicates nil)
+;;   (god-mode-all)
+;;   (define-key god-local-mode-map (kbd "i") #'god-local-mode)
+;;   (global-set-key (kbd "<escape>") #'(lambda () (interactive) (god-local-mode 1)))
+;;   (define-key god-local-mode-map (kbd ".") #'repeat))
 
 
 (use-package flycheck
-  :ensure t)
+  :ensure t
+  ;; :config
+  ;; (add-hook 'after-init-hook #'global-flycheck-mode)
+  )
 
-(use-package org-roam
-  :ensure t)
+;; (use-package org-roam
+;;   :ensure t
+;;   :custom
+;;   (org-roam-directory "~/roamBank")
+;;   (org-roam-completion-everywhere t)
+;;   :bind (("C-c o e" . org-roam-buffer-toggle)
+;;          ("C-c o s" . org-roam-node-find)
+;;          ("C-c o i" . org-roam-node-insert))
+;;   :config
+;;   (org-roam-setup))
+
 (use-package yasnippet
   :ensure t
   :init
   (yas-global-mode 1))
-(use-package yasnippet-snippets
+
+;; (use-package yasnippet-snippets
+;;   :ensure t)
+
+(use-package meow
+  :ensure t
+  :init
+  (defun meow-setup ()
+  ;; (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-overwrite-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)
+   '(":" . execute-extended-command)
+   '("%" . meow-query-replace-regexp)))
+  :config
+  (meow-setup)
+  (meow-global-mode 1))
+
+(with-eval-after-load 'cc-mode
+  (define-key c-mode-base-map (kbd "TAB") #'indent-for-tab-command))
+
+(use-package exec-path-from-shell
+  :init
+  (exec-path-from-shell-copy-envs '("LIBRARY_PATH" "INFOPATH" "CPATH" "MANPATH"))
+  (exec-path-from-shell-initialize))
+
+(use-package vundo
   :ensure t)
+
+(require 'dash)
+(add-hook 'org-mode-hook #'visual-line-mode)
+(add-hook 'eglot-managed-mode-hook (lambda () (flymake-mode -1)))
+(add-hook 'after-change-major-mode-hook 'meow-normal-mode)
+
+(use-package eglot-booster
+  :after eglot
+  :config (eglot-booster-mode))
+
+(use-package flycheck-eglot
+  :ensure t
+  :after (flycheck eglot)
+  :config
+  (global-flycheck-eglot-mode 1))
+
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '((rust-ts-mode rust-mode) .
+                 ("rust-analyzer" :initializationOptions
+                  (:check (:command "clippy"))))))
+
+(use-package surround
+  :ensure t
+  :bind (("C-c s i" . surround-insert)
+         ("C-c s d" . surround-delete)
+         ("C-c s y" . surround-kill)))
+
+(setq shell-command-switch "-ic")
+
+(defun org-home ()
+  (interactive)
+  (find-file "~/org/main.org"))
+
+(defun to-config ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(use-package embark
+  :ensure t
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :ensure t)
+
+(use-package vertico-prescient
+  :ensure t
+  :after vertico prescient
+  :custom
+  (vertico-prescient-enable-sorting t)
+  (vertico-prescient-override-sorting nil)
+  (vertico-prescient-enable-filtering nil)
+  :config
+  (vertico-prescient-mode 1))
+
+(use-package org-appear
+  :ensure t
+  :init
+  (add-hook 'org-mode-hook 'org-appear-mode))
+
+(use-package savehist
+  :init
+  (savehist-mode t))
